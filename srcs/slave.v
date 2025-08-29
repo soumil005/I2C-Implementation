14df1c133f2a0c00d74360d517724e3bf6adeb0f
Soumil Sethi <144139092+soumil005@users.noreply.github.com>
2025-08-29 16:51:55 +0530

    Update slave.v

--- srcs/slave.v
+++ srcs/slave.v
@@ -1,103 +1,147 @@
 `timescale 1ns / 1ps
 
-module slave(sda_s, scl_s);
-    parameter RADDR = 3'b000, ACK = 3'b001, RDATA = 3'b010, ACK_2 = 3'b011;
-    parameter address = 7'b1101111;
-  
-    inout sda_s;
-    inout scl_s;
+module slave(scl, sda);
     
-    reg sda;
-   
-    reg [2:0] state = 0;
-    reg [7:0] data_reg; 
-    reg [7:0] address_reg;
+    input scl;
+    inout sda;
     
-    reg [3:0] count = 4'b1000;
-    reg m; //matching address
-    reg write_enable = 0;
-    reg start = 0;
+    parameter slave_addr = 7'b1100100;
     
-    assign sda_s = (write_enable == 1) ? sda : 'bz;
+    reg [7:0] address;
+    reg [3:0] addrCounter;
+    reg [3:0] dataCounter;
+    reg [2:0] ps;
+    reg start = 1'b0;
     
-    always@(negedge sda_s) begin
-        if(start == 0 && scl_s == 1) begin
-            start <= 1;
-            count <= 4'b1000;
+    reg [7:0] mdata;
+    reg [7:0] sdata = 8'd170;
+    
+    reg enable = 1'b0;
+    reg sda_s;
+    
+    assign sda = enable ? sda_s == 1'b0 ? 1'b0 : 1'bz : 1'bz;
+    
+    parameter RADDR = 3'd0, ACK = 3'd1, RDATA = 3'd2, WDATA = 3'd3, ACK2 = 3'd4;
+    
+    reg count;
+    
+    always@(negedge sda) begin
+        if(scl == 1'b1 && start == 1'b0) begin
+            start <= 1'b1;
+            ps <= RADDR;
+            addrCounter <= 4'd8;
+            enable <= 1'b0;
+            count <= 1'b0;
         end
     end
     
-    always @(posedge sda_s) begin
-		if (start == 1 && scl_s == 1) begin
-			state <= RADDR;
-			start <= 0;
-			write_enable <= 0;
-		end
-	end
+    always@(posedge sda) begin
+        if(scl == 1'b1 && start == 1'b1) begin
+            start <= 1'b0;
+            ps <= RADDR;
+            enable <= 1'b0;
+        end
+    end
     
-    always@(posedge scl_s) begin
-        if(start == 1) begin
-            case(state)
+    always@(posedge scl) begin
+        if(start) begin
+            case(ps)
                 RADDR: begin
-                    if(count != 4'b0000) begin
-                        address_reg[count - 1] <= sda_s;
-                        count <= count - 1;
+                    if(count == 1'b1) begin
+                        if(addrCounter == 4'd0) begin
+                            ps <= ACK;
+                            
+                        end 
+                        else begin
+                            address[addrCounter - 1] <= sda;
+                            addrCounter <= addrCounter - 1;
+                            ps <= RADDR;
+                            
+                        end
                     end
-                    
-                    else if(count == 4'b0000) begin
-                        state <= ACK;
-                    end
+                    else count <= count + 1;
+       
                 end
                 
                 ACK: begin
-                    if(address_reg[7:1] == address) begin
-                        count <= 4'b1000;
-                        state <= RDATA;
+                    if(address[7:1] == slave_addr) begin
+                        dataCounter <= 4'd8;
+                        if(address[0]) ps <= WDATA;
+                        else ps <= RDATA;
+                    end
+                    else begin
+                        ps <= RADDR; 
                     end
                 end
                 
                 RDATA: begin
-                    if(count != 4'b0000) begin
-                        data_reg[count - 1] <= sda_s;
-                        count <= count - 1;
+                    if(dataCounter == 4'd0) begin
+                        ps <= ACK2;
+                    end 
+                    else begin
+                        mdata[dataCounter - 1] <= sda;
+                        dataCounter <= dataCounter - 1;
+                        ps <= RDATA;
                     end
-                    else if(count == 4'b0000) begin
-						state <= ACK_2;
-					end
-                     
                 end
                 
-                ACK_2: begin
-                    state <= RADDR;
+                WDATA: begin
+                    if(dataCounter == 4'd0) begin
+                        ps <= ACK2;
+                    end 
+                    else begin
+                        dataCounter <= dataCounter - 1;
+                        ps <= WDATA;
+                    end
                 end
                 
+                ACK2: begin
+                
+                    if(address[0] && sda == 1'b0) begin
+                        addrCounter <= 4'd8;
+                        ps <= RADDR;
+                    end
+                    else if(!address[0]) begin
+                        addrCounter <= 4'd8;
+                        ps <= RADDR;
+                    end
+                    else ps <= ACK2;
+                end
             endcase
         end
-        
     end
     
-    always@(negedge scl_s) begin
-        case(state)
-            RADDR: begin
-                write_enable <= 1'b0;
-            end
-            
-            ACK: begin
-                sda <= 1'b0;
-                write_enable <= 1'b1;
-            end
-            
-            RDATA: begin
-                write_enable <= 1'b0;
-            end
-            
-            ACK_2: begin
-                sda <= 1'b0;
-                write_enable <= 1'b1;
-            end
-            
-        endcase
-        
+    always@(negedge scl) begin
+        if(start) begin
+            case(ps)
+                RADDR: begin
+                    enable <= 1'b0;
+                end
+                
+                ACK: begin
+                    enable <= 1'b1;
+                    if(address[7:1] == slave_addr) sda_s <= 1'b0;
+                    
+                end
+                
+                RDATA: begin
+                    enable <= 1'b0;
+                end
+                
+                WDATA: begin
+                    enable <= 1'b1;
+                    sda_s <= sdata[dataCounter - 1];
+                end
+                
+                ACK2: begin
+                    if(!address[0]) begin
+                        enable <= 1'b1;
+                        sda_s <= 1'b0;
+                    end
+                    else enable <= 1'b0;
+                end
+            endcase
+        end
     end
     
 endmodule
